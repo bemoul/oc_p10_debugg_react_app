045e5ed28e120f326d733d489ada14926e8f93ef
Selim-Ramdani <selimcodeur@gmail.com>
2024-05-23 07:50:54 +0200

    Add event type filtering to EventList component

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,10 +13,12 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
+  // filter events by type and pagination
   const filteredEvents = (
     (!type
       ? data?.events
-      : data?.events) || []
+      : data?.events.filter((event) => event.type === type)
+    ) || []
   ).filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
