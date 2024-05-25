921af170c46e17892713b2fdb6b91d657322f648
Selim-Ramdani <selimcodeur@gmail.com>
2024-05-25 11:39:35 +0200

    add findByText() form the message text in the form

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -4,10 +4,11 @@ import Home from "./index";
 describe("When Form is created", () => {
   it("a list of fields card is displayed", async () => {
     render(<Home />);
-    await screen.findByText("Email");
     await screen.findByText("Nom");
     await screen.findByText("Prénom");
     await screen.findByText("Personel / Entreprise");
+    await screen.findByText("Email");
+    await screen.findByText("Message");
   });
 
   describe("and a click is triggered on the submit button", () => {
