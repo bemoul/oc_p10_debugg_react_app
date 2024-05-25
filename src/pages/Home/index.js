16ae546c5a3dc9e82311fcbdc0be48b0d99e313e
Selim-Ramdani <selimcodeur@gmail.com>
2024-05-25 08:03:43 +0200

    Optimized section IDs for improved page navigation in Page component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,7 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const { last } = useData()
   return <>
     <header>
       <Menu />
@@ -22,7 +22,7 @@ const Page = () => {
       <section className="SliderContainer">
         <Slider />
       </section>
-      <section className="ServicesContainer">
+      <section className="ServicesContainer" id="nos-services">
         <h2 className="Title">Nos services</h2>
         <p>Nous organisons des événements sur mesure partout dans le monde</p>
         <div className="ListContainer">
@@ -51,11 +51,11 @@ const Page = () => {
           </ServiceCard>
         </div>
       </section>
-      <section className="EventsContainer">
+      <section className="EventsContainer" id="nos-realisations">
         <h2 className="Title">Nos réalisations</h2>
         <EventList />
       </section>
-      <section className="PeoplesContainer">
+      <section className="PeoplesContainer" id="notre-equipe">
         <h2 className="Title">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
@@ -117,12 +117,12 @@ const Page = () => {
       <div className="col presta">
         <h3>Notre dernière prestation</h3>
         <EventCard
-            imageSrc={`${last?.cover}`}
-            title={`${last?.title}`}
-            date={new Date(last?.date)}
-            small
-            label={`${last?.type}`}
-            data-testid="last-event"
+          imageSrc={`${last?.cover}`}
+          title={`${last?.title}`}
+          date={new Date(last?.date)}
+          small
+          label={`${last?.type}`}
+          data-testid="last-event"
         />
       </div>
       <div className="col contact">
